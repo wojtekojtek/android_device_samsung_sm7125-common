60bfd278acb1d9a63ca11269b9f8be69096aec7c
Bruno Martins <bgcngm@gmail.com>
2024-02-08 17:05:32 +0000

    sm7125-common: Use the common device compatibility matrix from QCOM
    Change-Id: I0c0f3ecddbe6d7b232cc0d13c78ca58094e4e720

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -161,7 +161,7 @@ TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x2000U | 0x400000000LL
 
 # HIDL manifests
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/manifest.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/configs/framework_compatibility_matrix.xml \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
